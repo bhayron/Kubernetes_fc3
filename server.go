3176c1917eb240c7ca596cd4e33cfd7e8f5227d9
Bhayron Klivilan <bhayronklivilan@gmail.com>
2022-08-26 01:21:11 -0300

    feat: :recycle:

---
+++ server.go
@@ -0,0 +1,12 @@
+package main
+
+import "net/http"
+
+func main(){
+	http.HandleFunc("/", Helo)
+	http.ListenAndServe(":88", nil)
+}
+
+func Helo(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("<h1>Hello Full Cycle</h1>"))
+}
