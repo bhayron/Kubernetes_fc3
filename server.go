549c4a870ada387bc02b92256b0ce4b97cd6ca3e
Bhayron Klivilan <bhayronklivilan@gmail.com>
2022-08-30 22:36:23 -0300

    feat: :art:

--- server.go
+++ server.go
@@ -1,12 +1,13 @@
 package main
 
+//v1
 import "net/http"
 
-func main(){
+func main() {
 	http.HandleFunc("/", Helo)
 	http.ListenAndServe(":88", nil)
 }
 
-func Helo(w http.ResponseWriter, r *http.Request){
+func Helo(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("<h1>Hello Full Cycle</h1>"))
 }
